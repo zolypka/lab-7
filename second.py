068174989d5637189107cfa150dd83d716aa2247
zolypka <161860029+zolypka@users.noreply.github.com>
2024-04-27 11:36:43 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,20 +1,16 @@
-def find_max_substring(search_query):
-    search_query = search_query.lower()
-    max_substring = ""
-    max_count = 0
+def find_mm(str):
+    bfint=0
+    bfstr=''
+    for i in range(1, len(str)):
+        for j in range(0,i):
+            if(str[j:i])>=bfint and len(bfstr)<len(str[j:i])):
+                bfint=str.count(str[j:i])
+                bfstr=str[j:i]
+            elif(str.count(str[j:i])>bfint):
+                bfint=str.count(str[j:i])
+                bfstr=str[j:i]
+    return bfint, bfstr        
 
-    for i in range(len(search_query)):
-        for j in range(i + 1, len(search_query) + 1):
-            substring = search_query[i:j]
-            count = search_query.count(substring)
 
-            if count > max_count or (count == max_count and len(substring) > len(max_substring)):
-                max_substring = substring
-                max_count = count
-
-    return max_substring, max_count
-
-user_input = input("write a word: ")
-max_substring, max_count = find_max_substring(user_input)
-
-print(f"max substring : {max_substring}, multiplicity: {max_count}")
+st = input()
+print(find_mm(st))
